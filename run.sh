e9485fc816df6cf3435779c844ba1b8273d92325
ColdFuzz <ColdFuzz186@users.noreply.github.com>
2025-12-21 15:03:06 +0000

    fixed desktoptemplate, fixed run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 # Move to the folder where this script lives
 cd "$(dirname "$0")"
 # Launch Love2D 
-SDL_VIDEODRIVER=x11 love .    #If you are using the flatpak version of Love2D, this needs changed so it will launch.
+love .    #If you are using the flatpak version of Love2D, this needs changed so it will launch.
 
